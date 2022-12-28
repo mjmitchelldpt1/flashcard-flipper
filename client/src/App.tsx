7c83ef0717f908a0fffc255645c3d863c82feb51
Mike Mitchell <mjmitchelldpt1@gmail.com>
2022-12-27 20:03:12 -0600

    working on showing decks and buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+type TypeDeck = {
+  title: string;
+  _id: string;
+};
+
 function App() {
+  const [title, setTitle] = useState<string>("");
+  const [decks, setDecks] = useState<TypeDeck[]>([]);
+
+  async function fetchDecks() {
+    const response = await fetch("http://localhost:5000/decks");
+    const data = await response.json();
+    setDecks(data);
+  }
+  useEffect(() => {
+    fetchDecks();
+  }, [decks]);
+
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault();
+    await fetch("http://localhost:5000/decks", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setTitle("");
+  }
   return (
     <div className="App">
-      <form>
-        <label htmlFor="">Deck Title</label>
-        <input type="text" />
+      <h1 className="">The Flippening</h1>
+      <ul className="decks">
+        {decks.map((deck) => (
+          <li className="" key={deck._id}>
+            <button className="">X</button>
+            {deck.title}
+          </li>
+        ))}
+      </ul>
+      <form className="" onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          id="deck-title"
+          type="text"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+        <button className="">Create Deck</button>
       </form>
     </div>
   );
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,26 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import Deck from "./models/Deck";
 import { config } from "dotenv";
 
 config();
 const PORT = 5000;
-
 const app = express();
+
+app.use(cors());
 // express pulls in ability to call JSON body in thunder client via EXPRESS MIDDLEWARE FUNCTION
 app.use(express.json());
 
+app.get("/decks", async (req: Request, res: Response) => {
+  // call data from mongoDB custom decks
+  const decks = await Deck.find();
+  //return data back to user
+  res.json(decks);
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
   //takes req.body in thundercloud to give newDeck to pass whatever title is given
-  console.log(req.body);
   const newDeck = new Deck({ title: req.body.title });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
